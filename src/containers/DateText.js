48c1c9b8c3256d7ddd77e3ab8232ba488ca5a528
Jason Lin <amigo_brian@hotmail.com>
2019-06-22 22:00:17 +0800

    新增About頁背景, 新增字型

--- src/containers/DateText.js
+++ src/containers/DateText.js
@@ -37,14 +37,14 @@ class DateText extends Component {
     getMainImg(main){
         switch(main){
             case 'Clouds':      //陰
-                return clouds;
+                return '\u2601'
             case 'Thunderstorm':    //雷
                 return storm;
             case 'Clear':       //晴
-                return sun;    
+                return '\u26c5'
             case 'Rain':        //雨
             case 'Drizzle':
-                return rain;     
+                return '\u2614' 
             case 'Mist':     //霧霾沙塵
             case 'Smoke': 
             case 'Haze': 
@@ -54,9 +54,9 @@ class DateText extends Component {
             case 'Ash': 
             case 'Squall': 
             case 'Tornado': 
-                return atmosphere;     
+                return '\ud83c\udf01'  
             default:            //其他
-                return alice;
+                return '';
         }
     }
 
@@ -67,8 +67,8 @@ class DateText extends Component {
                 <div className='timeText'>{currentTime.timeText}</div>
                 <div className='dateText'>{currentTime.dateText}</div>      
                 <div className='temp'>{currentWeather.temp}
-                    <span>
-                        <img className='mainImg' src={this.getMainImg(currentWeather.weather)} alt={currentWeather.weather}></img>
+                    <span className='weatherIcon'>
+                        {this.getMainImg(currentWeather.weather)}
                     </span>
                 </div>
                 
