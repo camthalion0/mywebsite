6c629b723d0154b70b0af9820a16551eedf7a7fc
camthalion0 <camthalion1234@gmail.com>
2019-06-26 15:31:33 +0800

    redux切換menu效果

--- src/components/MenuItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// const MenuItem = ()=>
-//         (
-//             <a className='MenuItem' src={this.props.link}>{this.props.title}</a>
-//         )
-  
-
-export default MenuItem;
